ec3653ec436a4c54c5d9691f58555ef13bb8f114
Isra1997 <israragheb@gmail.com>
2024-08-18 11:11:15 +0300

    initial commit

---
+++ src/main/java/com/elasticsearch/text_analyzer_designer/TextAnalyzerDesignerApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.text_analyzer_designer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalyzerDesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAnalyzerDesignerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elasticsearch/text_analyzer_designer/TextAnalyzerDesignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.text_analyzer_designer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAnalyzerDesignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
